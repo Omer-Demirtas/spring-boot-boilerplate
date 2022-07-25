b04ead79d03f517a8f0b2e152262809055eab3e8
OmerHalil <gamega55834@gmail.com>
2022-07-25 22:13:04 +0300

    Adding custom exception handler for blank post requests.

---
+++ src/main/java/com/boilerplate/config/RestExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.boilerplate.config;
+
+import com.boilerplate.dto.ApiResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler
+{
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex,
+            HttpHeaders headers, HttpStatus status,
+            WebRequest request
+        )
+    {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiResponse(HttpStatus.BAD_REQUEST, error));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiResponse apiResponse)
+    {
+        return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
+    }
+}
