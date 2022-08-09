f56be08363562feb276838fce4c94cf818e754f0
OmerHalil <gamega55834@gmail.com>
2022-08-09 23:48:36 +0300

    Logger Aspect updated.

--- src/main/java/com/boilerplate/aspect/Logger.java
+++ src/main/java/com/boilerplate/aspect/Logger.java
@@ -12,28 +12,37 @@ import java.util.Arrays;
 @Component
 public class Logger
 {
+    /*
     @Pointcut("@within(com.boilerplate.annotation.LogClass)")
     public void logClass() {}
 
     @Pointcut("@annotation(com.boilerplate.annotation.LogMethod)")
     public void logMethod() {}
+    */
 
-    @Pointcut("@annotation(com.boilerplate.annotation.NotLogMethod)")
+    @Pointcut("@annotation(com.boilerplate.annotation.NotLog)")
     public void notLogMethod() {}
 
-    @Before("(logClass() || logMethod()) && !notLogMethod()")
+    @Pointcut("@within(com.boilerplate.annotation.LogThrowClass)")
+    public void logThrowClass() {}
+
+    @Pointcut("@annotation(com.boilerplate.annotation.LogThrowMethod)")
+    public void logThrowMethod() {}
+
+    //@Before("(logClass() || logMethod()) && !notLogMethod()")
+    @Before("execution(* com.boilerplate.service.*.*(..)) && !notLogMethod()")
     public void adviceBefore(JoinPoint jp)
     {
         log.info(jp.toShortString() + Arrays.toString(jp.getArgs()) + " start...");
     }
 
-    @After("(logClass() || logMethod()) && !notLogMethod()")
+    @After("execution(* com.boilerplate.service.*.*(..)) && !notLogMethod()")
     public void adviceAfter(JoinPoint jp)
     {
         log.info(jp.toShortString() + Arrays.toString(jp.getArgs()) + " end...");
     }
 
-    @AfterThrowing(value = "(logClass() || logMethod()) && !notLogMethod()", throwing = "ex")
+    @AfterThrowing(value = "logThrowClass() || logThrowMethod()", throwing = "ex")
     public void adviceAfterThrowing(JoinPoint jp, Exception ex)
     {
         log.info("{}{} | {} {}", jp.toShortString(), Arrays.toString(jp.getArgs()), ex.getClass().getName(), ex.getMessage());
