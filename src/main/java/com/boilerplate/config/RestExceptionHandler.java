4a601900e1c93fb054478cab092b7008c6e5e263
Omer-Demırtas <omer.halil.dmrts@gmail.com>
2025-02-23 00:38:14 +0300

    RestExceptionHandler updated with handleBaseException method & javadoc added

--- src/main/java/com/boilerplate/config/RestExceptionHandler.java
+++ src/main/java/com/boilerplate/config/RestExceptionHandler.java
@@ -1,18 +1,32 @@
 package com.boilerplate.config;
 
+import com.boilerplate.exception.BaseException;
 import com.boilerplate.utils.GenericResponse;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@ControllerAdvice
+/**
+ * Global exception handler for handling custom exceptions across the application.
+ * <p>
+ * This class captures all exceptions that extend {@link BaseException} and provides
+ * a unified response format using {@link GenericResponse}. It ensures consistent error
+ * handling and logging.
+ * </p>
+ *
+ * @author Omer Demirtas
+ */
+@Log4j2
+@RestControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public GenericResponse<?> handleEntityNotValid(RuntimeException ex) {
-        // TODO fill error code and error message
-        return GenericResponse.error();
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<GenericResponse<?>> handleBaseException(BaseException ex) {
+        log.error(ex);
+
+        return ResponseEntity
+                .status(ex.getHttpStatus())
+                .body(ex.toGenericResponse());
     }
 }
